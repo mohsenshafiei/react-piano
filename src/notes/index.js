ab7e95ae04f2c22c612964ec5201fa6df122c842
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2019-12-26 13:04:52 +0800

    feat: add keymaps and complete sounds.

--- src/notes/index.js
+++ src/notes/index.js
@@ -1,14 +1,159 @@
-import A from './A.mp3';
-import Ab from './Ab.mp3';
-import B from './B.mp3';
-import Bb from './Bb.mp3';
-import C from './C.mp3';
-import D from './D.mp3';
-import Db from './Db.mp3';
-import E from './E.mp3';
-import Eb from './Eb.mp3';
-import F from './F.mp3';
-import G from './G.mp3';
-import Gb from './Gb.mp3';
+import A0 from './a0.ogg';
+import A1 from './a1.ogg';
+import A2 from './a2.ogg';
+import A3 from './a3.ogg';
+import A4 from './a4.ogg';
+import A5 from './a5.ogg';
+import A6 from './a6.ogg';
+import A7 from './a7.ogg';
 
-export default [C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B];
+import B0 from './b0.ogg';
+import B1 from './b1.ogg';
+import B2 from './b2.ogg';
+import B3 from './b3.ogg';
+import B4 from './b4.ogg';
+import B5 from './b5.ogg';
+import B6 from './b6.ogg';
+import B7 from './b7.ogg';
+
+import C0 from './c0.ogg';
+import C1 from './c1.ogg';
+import C2 from './c2.ogg';
+import C3 from './c3.ogg';
+import C4 from './c4.ogg';
+import C5 from './c5.ogg';
+import C6 from './c6.ogg';
+import C7 from './c7.ogg';
+
+import D1 from './d1.ogg';
+import D2 from './d2.ogg';
+import D3 from './d3.ogg';
+import D4 from './d4.ogg';
+import D5 from './d5.ogg';
+import D6 from './d6.ogg';
+import D7 from './d7.ogg';
+
+import E1 from './e1.ogg';
+import E2 from './e2.ogg';
+import E3 from './e3.ogg';
+import E4 from './e4.ogg';
+import E5 from './e5.ogg';
+import E6 from './e6.ogg';
+import E7 from './e7.ogg';
+
+import F1 from './f1.ogg';
+import F2 from './f2.ogg';
+import F3 from './f3.ogg';
+import F4 from './f4.ogg';
+import F5 from './f5.ogg';
+import F6 from './f6.ogg';
+import F7 from './f7.ogg';
+
+import G1 from './g1.ogg';
+import G2 from './g2.ogg';
+import G3 from './g3.ogg';
+import G4 from './g4.ogg';
+import G5 from './g5.ogg';
+import G6 from './g6.ogg';
+import G7 from './g7.ogg';
+
+
+import A_0 from './a-0.ogg';
+import A_1 from './a-1.ogg';
+import A_2 from './a-2.ogg';
+import A_3 from './a-3.ogg';
+import A_4 from './a-4.ogg';
+import A_5 from './a-5.ogg';
+import A_6 from './a-6.ogg';
+import A_7 from './a-7.ogg';
+
+import C_1 from './c-1.ogg';
+import C_2 from './c-2.ogg';
+import C_3 from './c-3.ogg';
+import C_4 from './c-4.ogg';
+import C_5 from './c-5.ogg';
+import C_6 from './c-6.ogg';
+import C_7 from './c-7.ogg';
+
+import D_1 from './d-1.ogg';
+import D_2 from './d-2.ogg';
+import D_3 from './d-3.ogg';
+import D_4 from './d-4.ogg';
+import D_5 from './d-5.ogg';
+import D_6 from './d-6.ogg';
+import D_7 from './d-7.ogg';
+
+import F_1 from './f-1.ogg';
+import F_2 from './f-2.ogg';
+import F_3 from './f-3.ogg';
+import F_4 from './f-4.ogg';
+import F_5 from './f-5.ogg';
+import F_6 from './f-6.ogg';
+import F_7 from './f-7.ogg';
+
+import G_1 from './g-1.ogg';
+import G_2 from './g-2.ogg';
+import G_3 from './g-3.ogg';
+import G_4 from './g-4.ogg';
+import G_5 from './g-5.ogg';
+import G_6 from './g-6.ogg';
+import G_7 from './g-7.ogg';
+
+export const notes = [
+  'A0', 'A_0', 'B0', 'C0', 'C_1', 'D1', 'D_1', 'E1', 'F1', 'F_1', 'G1', 'G_1',
+  'A1', 'A_1', 'B1', 'C1', 'C_2', 'D2', 'D_2', 'E2', 'F2', 'F_2', 'G2', 'G_2',
+  'A2', 'A_2', 'B2', 'C2', 'C_3', 'D3', 'D_3', 'E3', 'F3', 'F_3', 'G3', 'G_3',
+  'A3', 'A_3', 'B3', 'C3', 'C_4', 'D4', 'D_4', 'E4', 'F4', 'F_4', 'G4', 'G_4',
+  'A4', 'A_4', 'B4', 'C4', 'C_5', 'D5', 'D_5', 'E5', 'F5', 'F_5', 'G5', 'G_5',
+  'A5', 'A_5', 'B5', 'C5', 'C_6', 'D6', 'D_6', 'E6', 'F6', 'F_6', 'G6', 'G_6',
+  'A6', 'A_6', 'B6', 'C6', 'C_7', 'D7', 'D_7', 'E7', 'F7', 'F_7', 'G7', 'G_7',
+  'A7', 'A_7', 'B7', 'C7'
+];
+
+export const notesSounds = [
+  A0, A_0, B0, C0, C_1, D1, D_1, E1, F1, F_1, G1, G_1,
+  A1, A_1, B1, C1, C_2, D2, D_2, E2, F2, F_2, G2, G_2,
+  A2, A_2, B2, C2, C_3, D3, D_3, E3, F3, F_3, G3, G_3,
+  A3, A_3, B3, C3, C_4, D4, D_4, E4, F4, F_4, G4, G_4,
+  A4, A_4, B4, C4, C_5, D5, D_5, E5, F5, F_5, G5, G_5,
+  A5, A_5, B5, C5, C_6, D6, D_6, E6, F6, F_6, G6, G_6,
+  A6, A_6, B6, C6, C_7, D7, D_7, E7, F7, F_7, G7, G_7,
+  A7, A_7, B7, C7
+];
+
+export const keymapSounds = {
+  ['81']: A0,
+  ['87']: A_0,
+  ['69']: B0,
+  ['82']: C0,
+  ['84']: C_1,
+  ['89']: D1,
+  ['85']: D_1,
+  ['73']: E1,
+  ['79']: F1,
+  ['80']: F_1,
+  ['219']: G1,
+  ['221']: G_1,
+  ['220']: A1,
+  ['65']: A_1,
+  ['83']: B1,
+  ['68']: C1,
+  ['70']: C_2,
+  ['71']: D2,
+  ['72']: D_2,
+  ['74']: E2,
+  ['75']: F2,
+  ['76']: F_2,
+  ['186']: G2,
+  ['222']: G_2,
+  ['90']: A2,
+  ['88']: A_2,
+  ['67']: B2,
+  ['86']: C2,
+  ['66']: C_3,
+  ['78']: D3,
+  ['77']: D_3,
+  ['188']: E3,
+  ['190']: F3,
+  ['191']: F_3,
+}
